f38c1f091368d0cd269857c5eb67531491cd3767
Paula <paulanunocorte@gmail.com>
2020-08-20 22:12:30 +0200

    12 y 13 done

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -94,7 +94,9 @@ print(files)
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+import random
 
+random_lists  = [n for n in range(100) 
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
@@ -102,7 +104,8 @@ print(files)
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 
-
+flatten_list = [n for sl in list_of_lists for n in sl]
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -111,6 +114,8 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
+floats = [float(n) for sl in list_of_lists for n in sl]
+print(floats)
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
