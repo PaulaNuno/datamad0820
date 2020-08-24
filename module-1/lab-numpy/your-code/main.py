07b6c2902f1d84868a1ae10b67936be2e1312c12
Paula <paulanunocorte@gmail.com>
2020-08-24 22:27:47 +0200

    Final sin solucionar

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -33,15 +33,16 @@ print("b size", b.size)
 
 #8. Are you able to add a and b? Why or why not?
 
-###It's not possible to add them because they have different size
+###It's not possible to add them because they have different shape. They must be broadcastable to a common shape.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-c = (np.transpose(b).reshape((2, 3, 5))
+c = np.transpose(b).reshape((2, 3, 5))
+
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-d = np.add(b, a)
+     
+d = np.add(a, c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
@@ -54,32 +55,46 @@ e = a * c
 
 #13. Does e equal to a? Why or why not?
 
+comparison = np.array_equal(e, a)
+print(comparison)
 
-
+#They are equal because "e" came from multiplying "a" by "c" and "c" array is fully made by 1.
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
+#15. Now we want to label the values in d. First create an empty array "f" with 
+# 
+# the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-#15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
+f = np.empty([2, 3, 5])
 
 
 """
-#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+#16. Populate the values in f. For each value in d, if it's larger than d_min 
+# but smaller than d_mean, assign 25 to the corresponding value in f.
+If a value in d is larger than d_mean but smaller than d_max, assign 75 to the 
+corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
-
-
+for index, value in np.ndenumerate(d):
+        if (value > d_min) & (value < d_mean):
+                print(index, 25)
+        elif (value > d_mean) & (value < d_max):
+                print(index, 75)
+        else:
+                (value == d_mean):
+                print(index, 50)
+        if value in d_min):
+                print(index, 0)
+        
 """
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
